3139f778c000b50be4881abca41b7797c9a94cfd
Gabriela Victoria <gabrielavictoria91@gmail.com>
2022-08-17 17:54:27 -0300

    layout sem problemas

--- wordle.js
+++ wordle.js
@@ -1,5 +1,3 @@
-const palavrasValidas = ["arroz", "amora", "teste"];
-
 const palavraDoDia = "arroz";
 
 let linha = 1;
@@ -8,56 +6,29 @@ let entrada = [];
 const ouvinteDeTeclas = (event) => {
   let char = event.key.toUpperCase();
   let alfabeto = [
-    "A",
-    "B",
-    "C",
-    "D",
-    "E",
-    "F",
-    "G",
-    "H",
-    "I",
-    "J",
-    "K",
-    "L",
-    "M",
-    "N",
-    "O",
-    "P",
-    "Q",
-    "R",
-    "S",
-    "T",
-    "U",
-    "V",
-    "W",
-    "X",
-    "Y",
-    "Z",
-    "ENTER",
-    "BACKSPACE",
-  ];
+    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q',
+    'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'ENTER', 'BACKSPACE'];
 
+  // INVALIDAR PALAVRA
   if (!alfabeto.includes(char)) {
-    // INVALIDAR PALAVRA
     console.log("tecla inválida", char);
     return null;
   }
 
+  // REMOVE LETRA
   if (char == "BACKSPACE") {
-    // REMOVE LETRA
     entrada.pop();
     console.log(entrada);
     retiraLetra();
     return;
   }
 
+  // VALIDAR PALAVRA
   if (char == "ENTER") {
     if (entrada.length == 5) {
       validarEntrada();
       linha += 1;
     }
-    // VALIDAR PALAVRA
     return;
   }
 
@@ -72,6 +43,7 @@ const ouvinteDeTeclas = (event) => {
   exibeLetra(char);
 };
 
+
 function exibeLetra(letra) {
   let elId = `l${linha}c${entrada.length}`;
   const el = document.getElementById(elId);
@@ -81,11 +53,11 @@ function exibeLetra(letra) {
 function retiraLetra(letra) {
   let elId = `l${linha}c${entrada.length + 1}`;
   const el = document.getElementById(elId);
-
   el.textContent = "";
   console.log(entrada.length, "coluna");
 }
-
+// el.classList.add('<nome da classe'>) adicionar classe
+// Digite el.classlist. para ver as demais propriedades 
 function validarEntrada() {
   if ((entrada.length = 5 && entrada.join("") == palavraDoDia.toUpperCase())) {
     alert("correto");
